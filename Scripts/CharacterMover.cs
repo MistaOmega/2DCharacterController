12307951d0458120a977173faf3b68dbd891d3cf
Jack Nash <jack.nash6@gmail.com>
2020-05-23 16:02:23 +0100

    Add files via upload

---
+++ Scripts/CharacterMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float movement;
+    public Rigidbody2D rb = null;
+    public float moveSpeed = 5f;
+
+    [Range(1, 10)]
+    public float jumpVelocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent <Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * (Time.deltaTime * moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * (Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+        
+    }
+}
---
+++ Scripts/JumpScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    public float fallMult = 2.5f;
+    public float lowerJumpMult = 1.8f;
+
+    private Rigidbody2D _rigidbody2D;
+    public void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (_rigidbody2D.velocity.y < 0)
+        {
+            _rigidbody2D.velocity += Vector2.up * (Physics2D.gravity.y * (fallMult - 1) * Time.deltaTime);
+        } else if (_rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            _rigidbody2D.velocity += Vector2.up * (Physics2D.gravity.y * (lowerJumpMult - 1) * Time.deltaTime);
+        }
+
+    }
+}
